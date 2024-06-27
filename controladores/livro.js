e0f2363eed12fbceb7f9ac4d8dfe22797f5cdb63
Luana Caroliny <luana.oliveira@autaza.com>
2024-06-26 23:07:15 -0300

    retornando json de livros

--- controladores/livro.js
+++ controladores/livro.js
@@ -1,6 +1,9 @@
+const fs = require("fs")
+
 function getLivros(req, res) {
     try {
-        res.send("Rota de livros")
+        const livros = JSON.parse(fs.readFileSync("livros.json"))
+        res.send(livros)
     } catch (error) {
         res.status(500)
         res.send(error.message)
