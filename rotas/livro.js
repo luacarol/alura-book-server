cc64d7102a8671820363d22733e676328aab409c
Luana Caroliny <luana.oliveira@autaza.com>
2024-06-26 22:42:46 -0300

    rotas

--- rotas/livro.js
+++ rotas/livro.js
@@ -6,4 +6,16 @@ router.get('/', (req, res) => {
     res.send("Rota de livros")
 })
 
+router.post('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo POST')
+})
+
+router.patch('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo PATCH')
+})
+
+router.delete('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo DELETE')
+})
+
 module.exports = router
