a1db4a0349e152b658caf583306ae4a8ab6f48d2
Luana Caroliny <luana.oliveira@autaza.com>
2024-07-04 21:56:24 -0300

    delete function

--- servicos/livro.js
+++ servicos/livro.js
@@ -21,8 +21,29 @@ function insereLivro(livroNovo) {
     fs.writeFileSync("livros.json", JSON.stringify(novaListaDeLivros))
 }
 
+function modificaLivro(modificacoes, id) {
+    let livrosAtuais = JSON.parse(fs.readFileSync("livros.json"))
+    const indiceModificado = livrosAtuais.findIndex(livro => livro.id === id)
+
+    const conteudoMudado = { ...livrosAtuais[indiceModificado], ...modificacoes }
+
+    livrosAtuais[indiceModificado] = conteudoMudado
+
+    fs.writeFileSync("livros.json", JSON.stringify(livrosAtuais))
+}
+
+function deletaLivro(id) {
+    const livros = JSON.parse(fs.readFileSync("livros.json"));
+
+    const novaListaDeLivros = livros.filter(livro => livro.id !== id);
+
+    fs.writeFileSync("livros.json", JSON.stringify(novaListaDeLivros));
+}
+
 module.exports = {
     getTodosLivros,
     getLivroPorId,
-    insereLivro
+    insereLivro,
+    modificaLivro,
+    deletaLivro
 }
